fdfda7fe1001b6851d07c93c2a891cc35c3bad04
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-09-27 02:02:50 +0530

    generated .npy wordvectors

--- model.py
+++ model.py
@@ -2,5 +2,6 @@ import numpy as np
 def load_matrices():
 	q1_ids = np.load('q1_ids_matrix.npy')
 	q2_ids = np.load('q2_ids_matrix.npy')
-	print q1_ids[500]
-	print q2_ids[500]
+	print q1_ids[18000]
+	print q2_ids[18000]
+load_matrices()
