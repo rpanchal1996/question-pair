cf0f7b30a57c7541f319abaad01d2417beffb3ed
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-09-27 23:49:07 +0530

    defined model for question 1

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ def load_data_saved():
 		data = json.load(myfile)
 	return data
 #load_matrices()
-'''
+
 wordVectors = np.load('word_vectors.npy')
 print wordVectors.shape
 batchSize = 1
@@ -22,19 +22,19 @@ numClasses = 30
 iterations = 100000
 numDimensions = 300
 tf.reset_default_graph()
-input_data = tf.placeholder(tf.int32,[batchSize, maxSeqLength])
-labels = tf.placeholder(tf.float32, [batchSize, numClasses])
-data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
-data = tf.nn.embedding_lookup(wordVectors,input_data)
-lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
-lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
-value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
-weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
-bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
-value = tf.transpose(value, [1, 0, 2])
-last = tf.gather(value, int(value.get_shape()[0]) - 1)
-prediction = (tf.matmul(last, weight) + bias)
-'''
+
+tf.variable_scope('Inference',reuse=False):
+	input_data_q1 = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+	data_q1 = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
+	data_q1 = tf.nn.embedding_lookup(wordVectors,input_data)
+	weights_q1 = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+	bias_q1 = tf.Variable(tf.constant(0.1, shape=[numClasses]))	
+	lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+	lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+	value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+	last = tf.gather(value, int(value.get_shape()[0]) - 1)
+	prediction = (tf.matmul(last, weights_q1) + bias_q1)
+
 def load_question_pair():
 	global global_pair_counter
 	question_one_matrice = np.load('q1_ids_matrix.npy')
