8e75fb6b961daf8cff671d63c349b0f9a7179296
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-10-04 18:09:11 +0530

    added second model with stacked LSTM cells and batching

--- model2.py
+++ model2.py
@@ -23,43 +23,56 @@ def load_question_pair():
 	question_one_matrice = np.load('q1_ids_matrix.npy')
 	question_two_matrice = np.load('q2_ids_matrix.npy')
 	is_same_matrice = np.load('is_same_matrix.npy')
-	if np.sum(question_one_matrice[global_pair_counter]) == 0 or np.sum(question_one_matrice[global_pair_counter])==0:
-		global_pair_counter+=1
-		error = 1
-		question_one = question_one_matrice
-		question_two = question_two_matrice
-		is_same = is_same_matrice
-		return question_one, question_two, is_same, error
-	else:
-		try:
-			zero_index = question_one_matrice[global_pair_counter].tolist().index(0)
-			question_one = np.roll(question_one_matrice[global_pair_counter],30-zero_index)
-		except ValueError:
-			question_one = question_one_matrice[global_pair_counter]
+	
+	added = 0
+	question_one_batches = []
+	question_two_batches = []
+	is_same_batches = []
+	while added<8:
+		if np.sum(question_one_matrice[global_pair_counter]) == 0 or np.sum(question_one_matrice[global_pair_counter])==0 or len(is_same_matrice[global_pair_counter])>1:
+			global_pair_counter+=1
+			error = 1
+			question_one = question_one_matrice
+			question_two = question_two_matrice
+			is_same = is_same_matrice
+		else:
+			try:
+				zero_index = question_one_matrice[global_pair_counter].tolist().index(0)
+				question_one = np.roll(question_one_matrice[global_pair_counter],30-zero_index)
+			except ValueError:
+				question_one = question_one_matrice[global_pair_counter]
 
-		try:
-			zero_index = question_two_matrice[global_pair_counter].tolist().index(0)
-			question_two = np.roll(question_two_matrice[global_pair_counter],30-zero_index)
-		except ValueError:
-			question_two = question_two_matrice[global_pair_counter]
-		is_same = is_same_matrice[global_pair_counter]
-		global_pair_counter+=1
-		#print global_pair_counter
-		error = 0
-		#question_one = 
-		question_one = question_one.reshape(question_one.shape[0],-1).T
-		question_two = question_two.reshape(question_two.shape[0],-1).T
-		question_one[question_one==3999999] = 214476 
-		question_two[question_two==3999999] = 214476
-		return question_one,question_two,is_same,error
+			try:
+				zero_index = question_two_matrice[global_pair_counter].tolist().index(0)
+				question_two = np.roll(question_two_matrice[global_pair_counter],30-zero_index)
+			except ValueError:
+				question_two = question_two_matrice[global_pair_counter]
+			is_same = is_same_matrice[global_pair_counter]
+			global_pair_counter+=1
+			error = 0
+			question_one = question_one.reshape(question_one.shape[0],-1).T
+			question_two = question_two.reshape(question_two.shape[0],-1).T
+			question_one[question_one==3999999] = 214476 
+			question_two[question_two==3999999] = 214476
+			question_one_batches.append(question_one.flatten().tolist())
+			question_two_batches.append(question_two.flatten().tolist())
+			is_same_batches.append(is_same)
+			added+=1
 
-
-
-#load_matrices()
-
+			#print question_one.shape
+			#print question_two.shape
+			#print is_same.shape
+	#return question_one,question_two,is_same,error
+	question_one_final = np.array(question_one_batches)
+	question_two_final = np.array(question_two_batches)
+	is_same_final = np.array(is_same_batches)
+	#print question_one_final.shape
+	#print question_two_final.shape
+	#print is_same_final.shape
+	return question_one_final,question_two_final,is_same_final
 wordVectors = np.load('word_vectors.npy')
 print wordVectors.shape
-batchSize = 1
+batchSize = 8
 lstmUnits = 64
 numClasses = 30
 iterations = 100000
@@ -70,42 +83,7 @@ keep_prob = 0.75
 graph = tf.Graph()
 lstm_layers = 3
 with graph.as_default():
-	'''
-	label = tf.placeholder(tf.int32, [1], name='label')
-	with tf.variable_scope('Inference',reuse=False):
-		input_data_q1 = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
-		data_q1 = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
-		data_q1 = tf.nn.embedding_lookup(wordVectors,input_data_q1)
-		data_q1 = tf.cast(data_q1,tf.float32)
-		#weights_q1 = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
-		#bias_q1 = tf.Variable(tf.constant(0.1, shape=[numClasses]))	
-		weights_q1 = tf.get_variable('weights_q1',[lstmUnits, numClasses],initializer=tf.contrib.layers.xavier_initializer())
-		initialized = tf.constant(0.1, shape=[numClasses])
-		bias_q1 = tf.get_variable('bias_q1',initializer=initialized)
-		lstmCell_1 = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
-		lstmCell_1 = tf.contrib.rnn.DropoutWrapper(cell=lstmCell_1, output_keep_prob=0.75)
-		value_1, _ = tf.nn.dynamic_rnn(lstmCell_1, data_q1, dtype=tf.float32)
-		last_1 = tf.gather(value_1, int(value_1.get_shape()[0]) - 1)
-		prediction_1 = (tf.matmul(last_1, weights_q1) + bias_q1)
-			
-	#scope.reuse_variables()
-	with tf.variable_scope('Inference',reuse=True):
-		input_data_q2 = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
-		data_q2 = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
-		data_q2 = tf.nn.embedding_lookup(wordVectors,input_data_q2)
-		data_q2 = tf.cast(data_q2,tf.float32)
-		weights_q2 = tf.get_variable('weights_q1',shape=[lstmUnits, numClasses])
-		bias_q2 = tf.get_variable('bias_q1',shape=[numClasses])	
-		lstmCell_2 = tf.contrib.rnn.BasicLSTMCell(lstmUnits,reuse=tf.get_variable_scope().reuse)
-		lstmCell_2 = tf.contrib.rnn.DropoutWrapper(cell=lstmCell_2, output_keep_prob=0.75)
-		value_2, _ = tf.nn.dynamic_rnn(lstmCell_2, data_q2, dtype=tf.float32)
-		last_2 = tf.gather(value_2, int(value_2.get_shape()[0]) - 1)
-		prediction_2 = (tf.matmul(last_2, weights_q2) + bias_q2)
-	
-	'''
-	#GRAPH ATTEMPT 2 : 
-
-	label = tf.placeholder(tf.int32, [1], name='label')
+	label = tf.placeholder(tf.int32, [batchSize,1], name='label')
 	with tf.variable_scope('Inference',reuse=False):
 		input_data_q1 = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
 		#data_q1 = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
@@ -140,18 +118,18 @@ with graph.as_default():
 
 
 	#LOSS ATTEMPT 4
-	'''
-	print 'Q1  OP SHAPE== ' + str(question1_outputs.get_shape)
+	
 	diff = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(question1_outputs[:, -1, :], question2_outputs[:, -1, :])), reduction_indices=1))
 
 	margin = tf.constant(1.) 
 	labels = tf.to_float(label)
 	match_loss = tf.expand_dims(tf.square(diff, 'match_term'), 0)
+	
 	mismatch_loss = tf.expand_dims(tf.maximum(0., tf.subtract(margin, tf.square(diff)), 'mismatch_term'), 0)
 
-	loss = tf.add(tf.matmul(tf.expand_dims(labels,1), match_loss), tf.matmul(tf.expand_dims((1 - labels),1), mismatch_loss), 'loss_add')
+	loss = tf.add(tf.matmul(labels, match_loss), tf.matmul((1 - labels), mismatch_loss), 'loss_add')
 	final_loss = tf.reduce_mean(loss)
-	'''
+	
 	# LOSS FUNCTION ATTEMPT 3
 	'''
 	diff = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(question1_outputs, question2_outputs)), reduction_indices=1, keep_dims=True))
@@ -162,7 +140,7 @@ with graph.as_default():
 	loss = tf.add(tf.matmul(labels, match_loss), tf.matmul((1 - labels), mismatch_loss), 'loss_add')
 	final_loss = tf.reduce_mean(loss)
 	'''
-	
+	'''
 	#LOSS FUNCTION ATTEMPT 2
 	d = tf.reduce_sum(tf.square(question1_outputs-question2_outputs), 1)
 	label = tf.to_float(label)
@@ -170,7 +148,7 @@ with graph.as_default():
 	d_sqrt = tf.sqrt(d)
 	loss = label * tf.square(tf.maximum(0., margin - d_sqrt)) + (1 - label) * d
 	final_loss = 0.5 * tf.reduce_mean(loss)
-	
+	'''
 	
 	
 	
@@ -201,32 +179,17 @@ with graph.as_default():
 	
 	
 	for iteration_number in xrange(0,100000):
-		question_one,question_two,is_same, error = load_question_pair()
-		if error:
-			pass
-		else:
-			print iteration_number
-			loss_obtained = sess.run([final_loss], {input_data_q1: question_one, input_data_q2:question_two,label:is_same})
-			if iteration_number%50==0:
-				print 'LOSS AT STEP ' + str(iteration_number) + ' IS == ' +str(loss_obtained)
-			
-			if iteration_number%50 == 0 and iteration_number !=0:
-				summary = sess.run(loss_summary, {input_data_q1: question_one, input_data_q2:question_two,label:is_same})
-				writer.add_summary(summary, iteration_number)
-			if iteration_number%20000 == 0 and iteration_number !=0:
-				save_path = saver.save(sess, "models/siamese.ckpt", global_step=iteration_number)
-				print("saved to %s" % save_path)
+		question_one,question_two,is_same = load_question_pair()
+		print iteration_number
+		loss_obtained = sess.run([final_loss], {input_data_q1: question_one, input_data_q2:question_two,label:is_same})
+		if iteration_number%20==0:
+			print 'LOSS AT STEP ' + str(iteration_number) + ' IS == ' +str(loss_obtained)
+		
+		if iteration_number%50 == 0 and iteration_number !=0:
+			summary = sess.run(loss_summary, {input_data_q1: question_one, input_data_q2:question_two,label:is_same})
+			writer.add_summary(summary, iteration_number)
+		if iteration_number%20000 == 0 and iteration_number !=0:
+			save_path = saver.save(sess, "models/siamese.ckpt", global_step=iteration_number)
+			print("saved to %s" % save_path)
 
 	writer.close()
-
-
-'''
-for i in xrange(0,100000):
-	question_one,question_two,is_same,error = load_question_pair()
-	question_one = question_one.reshape(question_one.shape[0],-1).T
-	print question_one
-	question_one[question_one==3999999] = 214476 
-	print question_one
-	print is_same.shape
-	#print error
-'''
