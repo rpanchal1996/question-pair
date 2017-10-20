3e44382826c86f05e26b4023bd864bf5254ab097
Rudresh Panchal <rudreshpanchal11@gmail.com>
2017-10-20 22:27:31 +0530

    removed redundant files

--- preprocess.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-file_path = '/home/rudresh/Desktop/quora/train.csv'
-csv_dataframe  = pd.read_csv(file_path)
-csv_dataframe = csv_dataframe[['question1','question2','is_duplicate']]
